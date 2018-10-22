a06a4be00a3696c50cfeb5c6909004bbbf562dd4
daveywavey16 <daveywavey16@msn.com>
2018-10-22 13:15:48 -0700

    fixed display on order total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -61,11 +61,6 @@ function start (results) {
     });     
 };
 
-// Once the customer has placed the order, application should check if your store has enough 
-// of the product to meet the customer's request.
-// If not, the app should log a phrase like 'Insufficient quantity!' 
-//and then prevent the order from going through.
-
 
 // code to update number of units in db
 function updateUnits(itemID, NumUnits, stock_quantity){
@@ -86,7 +81,7 @@ function updateUnits(itemID, NumUnits, stock_quantity){
 }
 function OrderTotal(NumUnits, price){
 	
-	 console.log("Order Total $" + NumUnits * price);
+	console.log("Order Total $" + NumUnits * price);
 
 }
 
@@ -98,7 +93,7 @@ function CheckUnits(ItemID, NumUnits, price){
     //   console.log(results[0].stock_quantity);
     //   console.log(NumUnits);
     	let price = results[0].price;
-    	OrderTotal(NumUnits, price);
+    	
     	//console.log(price);
 
 
@@ -115,16 +110,13 @@ function CheckUnits(ItemID, NumUnits, price){
     }
     
 
-// if your store does have enough of the product, you should fulfill the customer's order.
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-
     else if (parseInt(NumUnits) <= results[0].stock_quantity) {
 
     // once you have the items, prompt the user for which they'd like to bid on
    
     updateUnits(ItemID, NumUnits, results[0].stock_quantity);
     
+    OrderTotal(NumUnits, price);
     	    
     };
         
